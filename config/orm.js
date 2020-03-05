b91da82760e68fa6f22f2447e651b7d036a7a02f
Ariel Khawly <akhawly59@gmail.com>
2020-03-04 18:21:49 -0800

    methods

--- config/orm.js
+++ config/orm.js
@@ -1,1 +1,32 @@
 let connection = require('connection.js');
+//select all method
+function selectAll() {
+    connection.connect(function (err) {
+        if (err) throw err;
+        connection.query("SELECT * FROM burgers", function (err, result, fields) {
+            if (err) throw err;
+            return result;
+        });
+    });
+};
+
+//insert method
+function insertOne() {
+    connection.connect(function (err) {
+        if (err) throw err;
+        connection.query("INSERT INTO burgers(burger_name, devoured) VALUES (?, ?)", function (err, result, fields) {
+            if (err) throw err;
+            return result;
+        });
+    });
+};
+//update method
+function updateOne() {
+    connection.connect(function (err) {
+        if (err) throw err;
+        connection.query("UPDATE burgers SET devoured = true", function (err, result, fields) {
+            if (err) throw err;
+            return result;
+        });
+    });
+};
