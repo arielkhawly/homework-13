5d7e958f91338e20e1ca0e2027513581288548ef
Ariel Khawly <akhawly59@gmail.com>
2020-03-17 18:18:35 -0700

    updated burger.js with the burger object and it's functions for all, create, and update + router

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -1,7 +1,48 @@
 let express = require('express');
-let app = express();
-let burger = require('./models/burger.js');
+let router = express.Router();
 
-// router
-app.get
-module.exports = router;
+let burger = require("../models/burger.js");
+
+
+router.get("/", function(req, res) {
+    burger.all(function(data) {
+      var hbsObject = {
+        burgers: data
+      };
+      console.log(hbsObject);
+      res.render("index", hbsObject);
+    });
+  });
+  
+  router.post("/", function(req, res) {
+    burger.create([
+      "burger_name", "devoured"
+    ], [
+      req.body.burger_name, req.body.devoured
+    ], function() {
+      res.redirect("/");
+    });
+  });
+  
+  router.put("/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
+  
+    console.log("condition", condition);
+  
+    burger.update({
+      devoured: req.body.devoured
+    }, condition, function() {
+      res.redirect("/");
+    });
+  });
+  
+  router.delete("/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
+  
+    burger.delete(condition, function() {
+      res.redirect("/");
+    });
+  });
+  
+
+  module.exports = router;
